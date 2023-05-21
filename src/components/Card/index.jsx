7a346511aa2aeacc7e925a90cd38300ab13c9316
Felipe Alves <107265059+not-felipe@users.noreply.github.com>
2023-05-21 18:17:00 -0300

    Update index.jsx

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -8,7 +8,7 @@ export default function Card() {
       <div>
         <h2>Pôster Blonde</h2>
         <p>Blonde (estilizado como blond) é o segundo álbum de estúdio do cantor e compositor norte-americano Frank Ocean. Foi lançado a 20 de Agosto de 2016 em exclusivo, inicialmente, na loja digital da iTunes e no serviço de streaming Apple Music como um acompanhamento ao álbum visual Endless, lançado no dia anterior sob as mesmas circunstâncias.</p>
-        <button>Comprar Agora!</button>
+        <a href="https://open.spotify.com/album/3mH6qwIy9crq0I9YQbOuDf?si=R5oNqOvrQz-SuDGojhtydw" target="_blank" className="button">Ouça Agora!</a>
       </div>
     </div>
   )
